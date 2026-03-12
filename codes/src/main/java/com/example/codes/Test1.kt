375c92d8206ea36be11332a648738b2484439f81
juu124 <juu12424@gmail.com>
2026-03-13 00:41:00 +0900

    [수학] 백준 간단한 수학 연산 코테

--- codes/src/main/java/com/example/codes/Test1.kt
+++ codes/src/main/java/com/example/codes/Test1.kt
@@ -1,45 +1,29 @@
 package com.example.codes
-
-// N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.
+// 백준 간단한 수학 문제 풀이
 fun main() {
-    val totalNumber = readln().toInt()  // 한줄 받아오기
-    val numbers = mutableListOf<Int>()
 
-    // 토탈 수만큼 데이터를 받아오기
-    repeat(totalNumber) {
-        // 데이터를 받아온다.
-        val num = readln().toInt()
+//    if (a + b <= 0) {
+//        // 프로그램 종료
+//        System.exit(0)
+//    } else {
+//        println(a + b)
+//    }
 
-        //  numbers라는 리스트에 하나하나 추가해준다.
-        numbers.add(num)
-    }
+//    val n = readln().toInt()
+//    var result = 0
+//    for (i in 1..n) {
+//        result += i
+//    }
+//    println(result)
 
-    // sort 시작
-    // case : bubble sort
-    val sort = bubbleSort(numbers.toIntArray())
-    for (i in sort) {
-        println(i)
-    }
-}
+//    val n = readln().toInt()
+//    repeat(n) {
+//        val (a, b) = readln().split(" ").map { it.toInt() }
+//        println(a + b)
+//    }
 
-fun bubbleSort(numbers: IntArray) : IntArray {
-    // 인접한 두 데이터를 비교한다.
-    // 리스트를 쭉 한번 돈다. 하나씩 살펴보면서,
-    // j번째 변수랑, j+1번째 변수를 비교해야한다.
-    for (i in numbers) {
-        for (j in 0 until numbers.size - 1) {
-            if (numbers[j] > numbers[j + 1]) {
-                // 둘이 swqp한다.
-                numbers.swap(j, j+1)
-            }
-        }
-    }
-    return numbers
-}
-
-fun IntArray.swap(i: Int, j: Int) {
-    //  swap : 리스트에 있는 두개의 요소들을 서로 교체해준다.
-    val temp = this[i]
-    this[i] = this[j]
-    this[j] = temp
+//    val n = readln().toInt()
+//    for(i in 1..9) {
+//        println("$n * $i = ${n*i}")
+//    }
 }
